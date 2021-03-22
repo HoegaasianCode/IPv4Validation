9b9570ac25b338478904bce5ed2ecb6b8a4b7a58
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-22 16:13:15 +0100

    Initial commit

---
+++ IPv4Validation/IPv4Validation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IPv4Validation
+{
+    public class IPv4Validation
+    {
+        private readonly string _ip;
+        private string[] IpArray { get; set; }
+        private bool IsValid { get; set; }
+
+        public IPv4Validation(string ip)
+        {
+            _ip = ip;
+            IsValid = true;
+        }
+        public void SplitToFields()
+        {
+            IpArray = _ip.Split('.');
+        }
+
+        public void IsIPv6()
+        {
+            if (_ip[3] == ':') IsValid = false;
+        }
+
+        public void IsLeadingZeroInField()
+        {
+            for(int i = 0; i < IpArray.Length; i++)
+            {
+                string field = IpArray[i];
+                if (field[0] == '0')
+                {
+                    IsValid = false;
+                    break;
+                }
+            }
+        }
+
+        public void IsCorrectFieldRange()
+        {
+            short x;
+            for(int i = 0; i < IpArray.Length; i++)
+            {
+                string field = IpArray[i];
+                x = Int16.Parse(field);
+                if (x >= 255 || x <= 1)
+                {
+                    IsValid = false;
+                    break;
+                }
+            }
+        }
+
+        public void IsLastIPDigitZero()
+        {
+            Array.Reverse(IpArray);
+            for(int i = IpArray.Length - 1; i >= 0; i--)
+            {
+                string lastField = IpArray[i];
+                if (lastField.Last() == '0') IsValid = false;
+            }
+        }
+
+        public bool IsIPv4Valid() => IsValid;
+
+    }
+}
---
+++ IPv4Validation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IPv4Validation
+{
+    class Program
+    {
+        // https://edabit.com/challenge/BNKRr4N2oFZQfrTY3
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            IPv4Validation ip = new ("201.137.43.53");
+            ip.SplitToFields();
+            ip.IsIPv6();
+            ip.IsLeadingZeroInField();
+            ip.IsCorrectFieldRange();
+            ip.IsLastIPDigitZero();
+            Console.Write(ip.IsIPv4Valid());
+        }
+    }
+}
