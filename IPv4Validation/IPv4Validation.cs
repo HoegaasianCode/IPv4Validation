33d1f67e4f2d50bcb6b53177a9b74e9c6da96eec
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-03-23 16:30:04 +0100

    Update IPv4Validation.cs

--- IPv4Validation/IPv4Validation.cs
+++ IPv4Validation/IPv4Validation.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System;
 
 namespace IPv4Validation
 {
     public class IPv4Validation
     {
         private readonly string _ip;
-        private string[] IpArray { get; set; }
+        private string[] IPv4Array { get; set; }
         private bool IsValid { get; set; }
 
         public IPv4Validation(string ip)
@@ -18,38 +13,28 @@ namespace IPv4Validation
             _ip = ip;
             IsValid = true;
         }
-        
+
         public void IsIPv6()
         {
-            if (_ip[5] == ':') IsValid = false;
-        }
-        
-        public void SplitToFields()
-        {
-            IpArray = _ip.Split('.');
+            if (_ip[4] == ':') IsValid = false;
+            else IPv4Array = _ip.Split('.');
         }
 
-        public void IsLeadingZeroInField()
-        {
-            for(int i = 0; i < IpArray.Length; i++)
-            {
-                string field = IpArray[i];
-                if (field[0] == '0')
-                {
-                    IsValid = false;
-                    break;
-                }
+        public void IsLeadingZeroInField()              // "0.0023.043.90"
+        {                                               // if a non-last index of a field is 0 :
+            for(int i = 0; i < IPv4Array.Length; i++)   // (1) the field contains a string of only 0's, (2) a lone "0", or (3), a leading 0.
+            {                                           // (1) is per definition not a leading 0, (2) can be checked by adding logical requirements,
+                string field = IPv4Array[i];            // leaving only (3)
             }
         }
 
         public void IsCorrectFieldRange()
         {
-            short x;
-            for(int i = 0; i < IpArray.Length; i++)
+            for(int i = 0; i < IPv4Array.Length; i++)
             {
-                string field = IpArray[i];
-                x = Int16.Parse(field);
-                if (x > 255 || x < 1)
+                string field = IPv4Array[i];
+                short t = Int16.Parse(field);
+                if (t > 255 || t <= 0)
                 {
                     IsValid = false;
                     break;
@@ -57,13 +42,21 @@ namespace IPv4Validation
             }
         }
 
-        public void IsLastIPDigitZero()
+        public void IsLastDigitInLastFieldZero() // "0.0123.043.90"
         {
-            Array.Reverse(IpArray);
-            for(int i = IpArray.Length - 1; i >= 0; i--)
+            for(int i = IPv4Array.Length - 1; i >= 0; i--)
             {
-                string lastField = IpArray[i];
-                if (lastField.Last() == '0') IsValid = false;
+                string lastField = IPv4Array[i];
+                for(int j = lastField.Length - 1; j >= 0; j--)
+                {
+                    char c = lastField[j];
+                    if (c == '0' && lastField.Length >= 1)
+                    {
+                        IsValid = false;
+                        i = -1;
+                        break;
+                    }
+                }
             }
         }
 
